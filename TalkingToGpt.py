54848c46b33aa70f5421be627ae13a46a533b06c
TheLemonDev <cursedlemone@Gmail.com>
2024-10-29 09:53:19 +1100

    Added code from an older project to this one!!!

--- TalkingToGpt.py
+++ TalkingToGpt.py
@@ -1,6 +1,56 @@
-from dotenv import load_dotenv
+import speech_recognition as sr
+import gtts
+import openai
+import keyboard
+import dotenv
 import os
 
-load_dotenv()
+listen_keybind = 'p'
 
-api_key = os.getenv("GPT_API_KEY")
+dotenv.load_dotenv()
+
+client = openai.OpenAI(
+    api_key=os.getenv("GPT_API_KEY")
+)
+
+recogniser = sr.Recognizer()
+
+def get_request(prompt: str, max_tokens: int, role: str):
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": role},
+                {"role": "user", "content": prompt},
+            ],
+            max_tokens=max_tokens,
+            model="gpt-3.5-turbo"
+        )
+        return(chat_completion.choices[0].message.content)
+    
+    except:
+        print("well obviously something went wrong but im too lazy to find out what errors I should check for so just check your internent or something :p")
+    return None
+
+def speech_to_string(sensitivity_adjustment_duration: int):
+    audio_data = sr.AudioData(b"", 16000, 2)
+    with sr.Microphone() as source:
+        print("Adjusting for ambient sound...")
+        recogniser.adjust_for_ambient_noise(source=source, duration=sensitivity_adjustment_duration)
+        print(f"Listening... (release {listen_keybind} to stop!)")
+        while True:
+            if keyboard.is_pressed(listen_keybind):
+                segment = recogniser.listen(source=source)
+                audio_data = sr.AudioData(audio_data.frame_data + segment.frame_data, segment.sample_rate, segment.sample_width)
+            else:
+                break
+            
+        try:
+            string = recogniser.recognize_google(audio_data)
+            print(f"RECOGNISED!: {string}")
+            
+        except sr.UnknownValueError:
+            print("UNRECOGNISED!")
+
+        except sr.RequestError:
+            print("OH NO SOMETHING BAD HAPPENED TO THE RECOGNISER RUN FOR YOUR LIFE")
+
