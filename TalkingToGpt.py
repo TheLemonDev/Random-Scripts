2868c3353205979f0f9b54073a13d10a8606b7fa
TheLemonDev <cursedlemone@Gmail.com>
2024-10-29 09:57:39 +1100

    Updated Script so it works now!!!

--- TalkingToGpt.py
+++ TalkingToGpt.py
@@ -28,22 +28,23 @@ def get_request(prompt: str, max_tokens: int, role: str):
         return(chat_completion.choices[0].message.content)
     
     except:
+        os.system('cls')
         print("well obviously something went wrong but im too lazy to find out what errors I should check for so just check your internent or something :p")
     return None
 
 def speech_to_string(sensitivity_adjustment_duration: int):
     audio_data = sr.AudioData(b"", 16000, 2)
     with sr.Microphone() as source:
+        os.system('cls')
         print("Adjusting for ambient sound...")
         recogniser.adjust_for_ambient_noise(source=source, duration=sensitivity_adjustment_duration)
+        os.system('cls')
         print(f"Listening... (release {listen_keybind} to stop!)")
-        while True:
-            if keyboard.is_pressed(listen_keybind):
-                segment = recogniser.listen(source=source)
-                audio_data = sr.AudioData(audio_data.frame_data + segment.frame_data, segment.sample_rate, segment.sample_width)
-            else:
-                break
-            
+        while keyboard.is_pressed(listen_keybind):
+            segment = recogniser.listen(source=source)
+            audio_data = sr.AudioData(audio_data.frame_data + segment.frame_data, segment.sample_rate, segment.sample_width)
+        
+        os.system('cls')
         try:
             string = recogniser.recognize_google(audio_data)
             print(f"RECOGNISED!: {string}")
@@ -53,4 +54,8 @@ def speech_to_string(sensitivity_adjustment_duration: int):
 
         except sr.RequestError:
             print("OH NO SOMETHING BAD HAPPENED TO THE RECOGNISER RUN FOR YOUR LIFE")
+
+while True:
+    if keyboard.is_pressed(listen_keybind):
+        speech_to_string(1)
 
