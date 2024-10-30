4f398c52aefaf5cb7a2c81528e5c5dc369fc2e83
TheLemonDev <cursedlemone@Gmail.com>
2024-10-31 10:33:42 +1100

    Reverted back to no gui :/

--- TalkingToGpt.py
+++ TalkingToGpt.py
@@ -5,11 +5,6 @@ import keyboard
 import dotenv
 import os
 import pygame
-from PyQt6.QtWidgets import *
-from PyQt6.QtGui import *
-from PyQt6.QtCore import *
-import sys
-import threading
 
 dotenv.load_dotenv()
 recogniser = sr.Recognizer()
@@ -31,61 +26,6 @@ roles = (
 
 listen_keybind = 'p'
 
-class Main_window(QMainWindow):
-	def __init__(self):
-		super().__init__()
-		self.setWindowTitle('GPT Home')
-		self.setGeometry(100, 100, 600, 400)
-		self.setStyleSheet("background-color: black;")
-		self.settings_active = False
-
-		self.central_widget = QWidget()
-		self.setCentralWidget(self.central_widget)
-
-		self.settings_widget = QWidget(self)
-		self.settings_widget.setFixedSize(200, 400)
-		self.settings_widget.setStyleSheet("background-color: rgba(30, 30, 30, .9);")
-		self.settings_widget.move(600, 0)
-
-		self.text = QLabel("HELLO WORLD TEST")
-		self.text.setStyleSheet("text-aligne: center;")
-
-		self.button = QPushButton(self)
-		self.button.setIcon(QIcon('settings_icon.png'))
-		self.button.setFixedSize(QSize(35, 35))
-		self.button.setIconSize(QSize(30, 30))
-
-		button_style = """
-					QPushButton {
-						border: none;
-						background-color: transparent;
-					}
-					QPushButton:hover {
-						background-color: rgba(255, 255, 255, .2);
-						border-radius: 10px;
-					}
-					QPushButton:pressed {
-						background-color: rgba(255, 255, 255, .3);
-						border-radius: 10px;
-					}
-					"""
-
-		self.button.setStyleSheet(button_style)
-		self.button.move(550, 10)
-		self.button.clicked.connect(self.move_settings)
-		self.button.raise_()
-
-	def change_colour(self, color):
-		self.setStyleSheet(f"background-color: {color};")
-
-	def move_settings(self):
-		self.settings_active = not self.settings_active
-		if self.settings_active:
-			self.settings_widget.move(600, 0)
-		else:
-			self.settings_widget.move(400, 0)
-		self.button.raise_()
-
 def get_request(prompt: str, max_tokens: int, role: str):
 	try:
 		chat_completion = client.chat.completions.create(
@@ -106,11 +46,9 @@ def get_request(prompt: str, max_tokens: int, role: str):
 def speech_to_string(sensitivity_adjustment_duration: int):
 	audio_data = sr.AudioData(b"", 16000, 2)
 	with sr.Microphone() as source:
-		window.change_colour("crimson")
 		os.system('cls')
 		print("Adjusting for ambient sound...")
 		recogniser.adjust_for_ambient_noise(source=source, duration=sensitivity_adjustment_duration)
-		window.change_colour("lawngreen")
 		os.system('cls')
 		print(f"Listening... (release {listen_keybind} to stop!)")
 		while True:
@@ -145,7 +83,6 @@ def speech_to_string(sensitivity_adjustment_duration: int):
 
 def string_to_speech(string: str):
 	try:
-		window.change_colour("cyan")
 		voice = gtts.gTTS(text=string, lang="en", slow=False, tld="com")
 		voice.save('voice.mp3')
 		pygame.mixer.init()
@@ -157,7 +94,6 @@ def string_to_speech(string: str):
 			pass
 		pygame.mixer.quit()
 		os.remove('voice.mp3')
-		window.change_colour("black")
 	
 	except:
 		print("TTS error!")
@@ -167,12 +103,4 @@ def listen_for_key():
 		if keyboard.is_pressed(listen_keybind):
 			speech_to_string(1)
 
-app = QApplication(sys.argv)
-
-window = Main_window()
-window.show()
-
-listener_thread = threading.Thread(target=listen_for_key, daemon=True)
-listener_thread.start()
-
-sys.exit(app.exec())
+listen_for_key()
